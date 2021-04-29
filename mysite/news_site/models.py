97e957544281c2e7905beae72393a4008d9bf060
OxiSgen <60621483+OxiSgen@users.noreply.github.com>
2021-04-29 22:39:41 +0300

    NEED MIGRATION!

--- mysite/news_site/models.py
+++ mysite/news_site/models.py
@@ -29,6 +29,7 @@ class News(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     news_text = models.CharField(max_length=200, blank=True, null=True)
     news_url = models.CharField(max_length=200, blank=True, null=True)
+    site_url = models.CharField(max_length=200, blank=True, null=True)
     news_hype_rate = models.PositiveSmallIntegerField(blank=True, null=True)
     pub_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
